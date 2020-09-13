37d7ec997060a5ce127cf5a9b785d0a27246c890
Ruben Villar <ruben@rubenvillar.net>
2020-09-13 16:51:26 +0200

    added hardcoded redirects

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.redirect("https://rubenvillar.net");
+});
+
+router.get("/:slug", function (req, res, next) {
+  const { slug } = req.params;
+  if (slug === "portfolio-wp") {
+    res.redirect(
+      "https://www.notion.so/Portfolio-WordPress-4c15e7418b154e7baf2d9f0b14253938"
+    );
+  } else if (slug === "mis-recetas-android") {
+    res.redirect(
+      "https://play.google.com/store/apps/details?id=net.rubenvillar.mis_recetas.twa"
+    );
+  } else {
+    res.redirect("https://rubenvillar.net");
+  }
 });
 
 module.exports = router;
