4a10b7ded2d90756d850daade70f74c2debef1ea
Ruben Villar <ruben@rubenvillar.net>
2020-09-16 12:43:25 +0200

    removed static assets

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
@@ -11,7 +10,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
